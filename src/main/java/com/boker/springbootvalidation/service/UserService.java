8e7c28e5bf4a9ffc173990c7ca8f37d26cfeaed5
bokerwere <josephboker93@gmail.com>
2023-02-07 23:16:32 -0800

    validation

--- src/main/java/com/boker/springbootvalidation/service/UserService.java
+++ src/main/java/com/boker/springbootvalidation/service/UserService.java
@@ -4,7 +4,6 @@ import com.boker.springbootvalidation.model.User;
 import com.boker.springbootvalidation.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.NoSuchElementException;
 
